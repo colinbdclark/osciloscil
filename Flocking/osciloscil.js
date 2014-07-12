f2e893eb926614be881ab0e87653674195b1a764
Adam Tindale <adam@adamtindale.com>
2014-07-12 14:56:04 -0400

    updating flocking example to use new builtin tablegenerator methods

--- Flocking/osciloscil.js
+++ Flocking/osciloscil.js
@@ -3,66 +3,54 @@ function osciloscil() {
     var that = {};
 
     var numberOfWaves = 1;
-    var buffersize = 1024;
+    var bufferSize = 1024;
     var waves;
 
+    // Superfluous since Flocking now provides access to tableGenerators
+    /*
     that.sine = (function(){
         var buffer = [];
-        for ( var i = 0; i < buffersize; i++){
-            buffer[i] = Math.sin(i / buffersize * 2 * Math.PI);
+        for ( var i = 0; i < bufferSize; i++){
+            buffer[i] = Math.sin(i / bufferSize * 2 * Math.PI);
         }
         return buffer;
     })();
 
     that.square = (function(){
         var buffer = [];
-        for ( var i = 0; i < buffersize; i++){
-            buffer[i] = (i < buffersize / 2) ? 1 : -1;
+        for ( var i = 0; i < bufferSize; i++){
+            buffer[i] = (i < bufferSize / 2) ? 1 : -1;
         }
         return buffer;
     })();
+    */
 
-    that.setwaves = function(wavestring){
+    /* Arguments can be a string, list of strings, or an array of strings */
+    that.setWaves = function (waves) {
+        waves = fluid.makeArray(waves);
+        var buffer = new Float32Array(bufferSize * waves.length);
 
-        var buffer = [];
-
-        if ( typeof wavestring === "string"  ){
-            numberOfWaves = 1;
-
-            buffer = that[wavestring]; 
-            
-            waves = wavestring;
-
-            return buffer;
+        for (var i = 0; i < waves.length; i++){
+            var temp = flock.fillTable(bufferSize, flock.tableGenerators[waves[i]]);
+            buffer.set(temp, bufferSize * i);
         }
 
-        if ( wavestring instanceof Array){
-            numberOfWaves = wavestring.length;
-
-            for ( var i = 0; i < wavestring.length; i++){
-                var temp = that[wavestring[i]];
-                buffer = buffer.concat(temp);
-            }
-
-            waves = wavestring;
-
-            return buffer;
-        }
-    }
+        return buffer;
+    };
 
     that.setfreq = function(f){
         that.synth.set("oscosc.freq", f/numberOfWaves);
-    }
+    };
 
     that.synth = flock.synth({
         synthDef: {
             id : "oscosc",
         ugen : "flock.ugen.osc",
         freq : 100,
-        table : that.setwaves(['sine','square']),
+        table : that.setWaves(['sin','square']),
         }
     });
 
     return that;
-}
+};
 
